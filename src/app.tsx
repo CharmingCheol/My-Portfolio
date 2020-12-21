82ba681a81530f0c9691adb96394c5b8d39b2bcd
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-12-21 16:49:49 +0900

    [삭제] : 임시 Temp 페이지, history module 삭제

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,6 @@ import StyleReset from "@utils/styles/reset";
 const Home = loadable(() => import("@pages/Home"));
 const Profile = loadable(() => import("@pages/Profile/reducer"));
 const Project = loadable(() => import("@pages/Project/reducer"));
-const Temp = loadable(() => import("@pages/Temp"));
 
 const App = () => {
   return (
@@ -17,7 +16,6 @@ const App = () => {
           <Route path="/" component={Home} exact />
           <Route path="/profile" component={Profile} exact />
           <Route path="/project" component={Project} exact />
-          <Route path="/temp" component={Temp} exact />
         </Switch>
       </BrowserRouter>
     </>
