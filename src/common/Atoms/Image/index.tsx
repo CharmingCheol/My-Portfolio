2be545a0200936965fb2c11be2315e8474977be7
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-09 20:18:54 +0900

    [추가] : 이미지 컴포넌트 추가

---
+++ src/common/Atoms/Image/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import SkeletonImage from "@static/img/skeletonImage.png";
+import useInterSection from "@hooks/useIntersection";
+import * as S from "./style";
+
+/**
+ * @param alt 이미지 에러가 발생할 때 대체로 보여지는 텍스트
+ * @param lazy lazy loading 적용 여부(default=false. true면 적용)
+ * @param src 이미지 경로
+ */
+
+export interface ImageProps {
+  alt: string;
+  className?: string;
+  lazy?: boolean;
+  src: string;
+}
+
+const Image = ({ alt, className, lazy = false, src }: ImageProps) => {
+  const [imgSrc, setImgSrc] = useState(lazy ? SkeletonImage : src);
+  const ref = useInterSection(() => {
+    setImgSrc(src);
+  });
+
+  return (
+    <>
+      <S.Image alt={alt} className={className} ref={lazy ? ref : null} src={imgSrc} />
+    </>
+  );
+};
+
+export default Image;
