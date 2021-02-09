5b4611646579df927d1c98ddcabed63dbd91bab9
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-09 20:19:14 +0900

    [추가] : palette.ts 추가

---
+++ src/utils/styles/palette.ts
@@ -0,0 +1,34 @@
+const palette = {
+  // brown
+  brown1: "#F9ECCD",
+  brown2: "#F4D79D",
+  brown3: "#E0B168",
+  brown4: "#C18940",
+  brown5: "#995711",
+  brown6: "#83430C",
+  brown7: "#6E3208",
+  brown8: "#582405",
+  brown9: "#491903",
+  // blue
+  blue1: "#C7CDF9",
+  blue2: "#929CF4",
+  blue3: "#5966E0",
+  blue4: "#303DC1",
+  blue5: "#000C99",
+  blue6: "#000983",
+  blue7: "#00066E",
+  blue8: "#000458",
+  blue9: "#000349",
+  // green
+  green1: "#D3F9C8",
+  green2: "#A0F494",
+  green3: "#60E05B",
+  green4: "#32C13A",
+  green5: "#029919",
+  green6: "#018321",
+  green7: "#016E26",
+  green8: "#005826",
+  green9: "#004926",
+};
+
+export default palette;
