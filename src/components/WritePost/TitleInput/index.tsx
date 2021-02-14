5f93f21518a8b5b096570aedf90da5c4e5071317
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-14 20:29:13 +0900

    [추가] : TitleInput 컴포넌트 추가

---
+++ src/components/WritePost/TitleInput/index.tsx
@@ -0,0 +1,27 @@
+import React, { memo, useCallback, useContext } from "react";
+import Textarea from "@common/Atoms/Textarea";
+import { MarkDownEditorContext } from "@reducers/MarkDownEditor";
+import { changeTitleAction } from "@reducers/MarkDownEditor/action";
+import * as S from "./style";
+
+const TitleInput = () => {
+  const { dispatch } = useContext(MarkDownEditorContext);
+
+  // 제목 변경
+  const changeTitle = useCallback(
+    (text: string) => {
+      dispatch(changeTitleAction(text));
+    },
+    [dispatch],
+  );
+
+  return (
+    <>
+      <S.Article>
+        <Textarea onChange={changeTitle} placeholder="제목을 입력하세요" />
+      </S.Article>
+    </>
+  );
+};
+
+export default memo(TitleInput);
