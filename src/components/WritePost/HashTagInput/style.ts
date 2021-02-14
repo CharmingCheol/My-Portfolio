66e5e476c93374eb89bb59c29fb7296314c53496
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-14 20:28:28 +0900

    [추가] : HashTagInput 컴포넌트 추가

---
+++ src/components/WritePost/HashTagInput/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Layout = styled.article`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 8px 0;
+  margin-bottom: 8px;
+  border-bottom: 1px solid gray;
+  input {
+    margin-left: 8px;
+  }
+  li {
+    margin-right: 8px;
+    margin-bottom: 8px;
+    cursor: pointer;
+  }
+`;
