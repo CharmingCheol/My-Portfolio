45d5a0615aa1fba6e90c285a5b3aa298685f6a6b
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-02-19 21:39:20 +0900

    [변경] : Image Component onClick 적용

--- src/common/Atoms/Image/index.tsx
+++ src/common/Atoms/Image/index.tsx
@@ -6,6 +6,7 @@ import * as S from "./style";
 /**
  * @param alt 이미지 에러가 발생할 때 대체로 보여지는 텍스트
  * @param lazy lazy loading 적용 여부(default=false. true면 적용)
+ * @param onClick 이미지 클릭 이벤트
  * @param src 이미지 경로
  */
 
@@ -13,18 +14,17 @@ export interface ImageProps {
   alt: string;
   className?: string;
   lazy?: boolean;
+  onClick?: () => void;
   src: string;
 }
 
-const Image = ({ alt, className, lazy = false, src }: ImageProps) => {
+const Image = ({ alt, className, lazy = false, onClick, src }: ImageProps) => {
   const [imgSrc, setImgSrc] = useState(lazy ? SkeletonImage : src);
-  const ref = useInterSection(() => {
-    setImgSrc(src);
-  });
+  const ref = useInterSection(() => setImgSrc(src));
 
   return (
     <>
-      <S.Image alt={alt} className={className} ref={lazy ? ref : null} src={imgSrc} />
+      <S.Image alt={alt} className={className} onClick={onClick} ref={lazy ? ref : null} src={imgSrc} />
     </>
   );
 };
