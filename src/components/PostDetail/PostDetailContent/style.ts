dacdcc1a42318988d8995753c452dc0088f36fef
CharmingCheol <toby0806@naver.com>
2021-07-23 00:33:55 +0900

    불필요한 components 폴더 삭제

--- src/components/PostDetail/PostDetailContent/style.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const Layout = styled.section`
-  padding-bottom: 24px;
-`;
--- src/components/Project/PhotoFrameList/style.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Layout = styled.ul`
-  width: 100vw;
-  height: 100vh;
-  transform-style: preserve-3d;
-`;
