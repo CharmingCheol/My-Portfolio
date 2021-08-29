7c11884d536e5c6db4b39de125299a2a4f35612c
CharmingCheol <toby0806@naver.com>
2021-08-29 21:30:51 +0900

    [refactor] : title, content state 제거

--- src/reducers/writeSlice.ts
+++ src/reducers/writeSlice.ts
@@ -1,59 +1,39 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import defaultThumbnail from "static/img/skeleton.png";
 
-interface Settings {
-  prevThumbnail: string;
-  /**
-   * 추후 공개/비공개, 해시태그 옵션 추가 예정
-   */
-}
-
-export interface CounterState extends Settings {
-  content: string;
-  thumbnail: string;
-  title: string;
+export interface CounterState {
+  prevThumbnail: string | null;
+  thumbnail: string | null;
 }
 
 const initialState: CounterState = {
-  content: "",
-  prevThumbnail: defaultThumbnail,
-  thumbnail: defaultThumbnail,
-  title: "",
+  prevThumbnail: null,
+  thumbnail: null,
 };
 
 export const writeSlice = createSlice({
   name: "write",
   initialState,
   reducers: {
-    changeContent: (state, action: PayloadAction<string>) => {
-      state.content = action.payload;
-    },
     changeThumbnail: (state, action: PayloadAction<string>) => {
       state.thumbnail = action.payload;
     },
-    changeTitle: (state, action: PayloadAction<string>) => {
-      state.title = action.payload;
+    claerAll: (state) => {
+      state.prevThumbnail = null;
+      state.thumbnail = null;
     },
     clearSettings: (state) => {
-      state.thumbnail = defaultThumbnail;
+      state.thumbnail = null;
     },
     undoSettings: (state) => {
       state.thumbnail = state.prevThumbnail;
     },
-    updatePrevSettings: (state, action: PayloadAction<Settings>) => {
+    updatePrevSettings: (state, action: PayloadAction<Partial<CounterState>>) => {
       const { prevThumbnail } = action.payload;
-      state.prevThumbnail = prevThumbnail;
+      if (prevThumbnail) state.prevThumbnail = prevThumbnail;
     },
   },
 });
 
-export const {
-  changeContent,
-  changeThumbnail,
-  changeTitle,
-  clearSettings,
-  undoSettings,
-  updatePrevSettings,
-} = writeSlice.actions;
+export const { changeThumbnail, claerAll, clearSettings, undoSettings, updatePrevSettings } = writeSlice.actions;
 
 export default writeSlice.reducer;
