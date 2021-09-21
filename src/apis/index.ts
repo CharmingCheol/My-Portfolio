4f752a3802d026366c01ca8f1ccdb5c7a2f6eef6
CharmingCheol <toby0806@naver.com>
2021-09-21 20:49:50 +0900

    fix : api 함수 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export { postContentImages, postThumbnailImages } from "./images";
-export { getWriting, getWritingList, postWriting } from "./writings";
+export { getWriting, getWritingList, postWriting, patchWriting, deleteWriting } from "./writings";
 
 export const mainAxios = axios.create({
   baseURL: `http://localhost:3001/api`,
--- src/apis/writings.ts
+++ src/apis/writings.ts
@@ -1,22 +1,43 @@
 import { AxiosRequestConfig } from "axios";
 import { mainAxios } from "./index";
 
-export const getWriting = (config: AxiosRequestConfig) => {
+export const getWriting = (id: number, config: AxiosRequestConfig) => {
   return mainAxios({
     method: "GET",
-    url: "/writings",
+    url: `/writings/${id}`,
     ...config,
   });
 };
 
-export const getWritingList = (now: number, size: number) => {
-  return mainAxios.get(`/writings?now=${now}&size=${size}`);
+export const getWritingList = (pagination: any, config: AxiosRequestConfig) => {
+  const { now, size } = pagination;
+  return mainAxios({
+    method: "GET",
+    url: `/writings?now=${now}&size=${size}`,
+    ...config,
+  });
 };
 
 export const postWriting = (config: AxiosRequestConfig) => {
   return mainAxios({
     method: "POST",
     url: "/writings",
+    ...config,
+  });
+};
+
+export const patchWriting = (id: number, config: AxiosRequestConfig) => {
+  return mainAxios({
+    method: "PATCH",
+    url: `/writings/${id}`,
+    ...config,
+  });
+};
+
+export const deleteWriting = (id: number, config: AxiosRequestConfig) => {
+  return mainAxios({
+    method: "DELETE",
+    url: `/writings/${id}`,
     ...config,
   });
 };
