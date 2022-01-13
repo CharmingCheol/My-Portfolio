a40690452ad6ad1b5e77537f23cac9cb3d7472ee
CharmingCheol <toby0806@naver.com>
2022-01-14 00:19:37 +0900

    remove : 기존 api 함수 제거

--- src/apis/index.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export { postContentImages, postThumbnailImages } from "./images";
-export { getWriting, getWritingList, postWriting, patchWriting, deleteWriting } from "./writings";
-
-export const mainAxios = axios.create({
-  baseURL: `http://localhost:3001/api`,
-});
