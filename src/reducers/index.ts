5f02edbb27158e8fe5774178795293ff0fd24668
CharmingCheol <toby0806@naver.com>
2022-02-14 00:40:08 +0900

    refactor: modal reducer state 추가

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+
+import globalUI from "./globalUI";
 import optionSlice from "./optionSlice";
 
-const reducer = combineReducers({ option: optionSlice });
+const reducer = combineReducers({ option: optionSlice, globalUI });
 
 export default reducer;
