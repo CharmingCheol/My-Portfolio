eea22cc10b98aec5564ddf114037e9150f9485d1
CharmingCheol <toby0806@naver.com>
2022-04-06 21:18:43 +0900

    feat: webpack 파일을 공통/dev/prod로 분리

---
+++ webpack.config.ts
@@ -0,0 +1,57 @@
+import path from "path";
+import webpack from "webpack";
+import Dotenv from "dotenv-webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import FaviconsWebpackPlugin from "favicons-webpack-plugin";
+
+const config: webpack.Configuration = {
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      common: path.resolve(__dirname, "src/common/"),
+      components: path.resolve(__dirname, "src/components/"),
+      containers: path.resolve(__dirname, "src/containers/"),
+      fireConfig: path.resolve(__dirname, "src/fireConfig/"),
+      hooks: path.resolve(__dirname, "src/hooks/"),
+      pages: path.resolve(__dirname, "src/pages/"),
+      reducers: path.resolve(__dirname, "src/reducers/"),
+      static: path.resolve(__dirname, "src/static/"),
+      store: path.resolve(__dirname, "src/store/"),
+      styles: path.resolve(__dirname, "src/styles/"),
+      types: path.resolve(__dirname, "src/types/"),
+      utils: path.resolve(__dirname, "src/utils/"),
+    },
+  },
+  entry: "./src/index",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        options: { transpileOnly: true },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "file-loader",
+        options: { name: "static/[contenthash].[ext]" },
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({ systemvars: true }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new FaviconsWebpackPlugin("./public/logo.png"),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
+};
+
+export default config;
