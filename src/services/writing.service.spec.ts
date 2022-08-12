127f45976e04378ec95e41b8e833f431a9089be1
CharmingCheol <toby0806@naver.com>
2022-08-12 23:26:47 +0900

    createWriting - 테스트 코드 중복 제거

--- src/services/writing.service.spec.ts
+++ src/services/writing.service.spec.ts
@@ -4,22 +4,21 @@ import WritingsServiceImpl from "./writing.service";
 jest.mock("../api");
 
 describe("writingService", () => {
+  let service: WritingsServiceImpl;
+
+  beforeEach(() => {
+    const mocked = jest.mocked(writingsApi, true);
+    service = new WritingsServiceImpl(mocked);
+  });
+
   describe("createWriting", () => {
     it("title이 빈 문자열인 경우 바로 리턴한다", () => {
-      const mocked = jest.mocked(writingsApi, true);
-      const service = new WritingsServiceImpl(mocked);
-
       const result = service.createWriting({ title: "", content: "content" });
-
       expect(result).toBeUndefined();
     });
 
     it("content가 빈 문자열인 경우 바로 리턴한다", () => {
-      const mocked = jest.mocked(writingsApi, true);
-      const service = new WritingsServiceImpl(mocked);
-
       const result = service.createWriting({ title: "title", content: "" });
-
       expect(result).toBeUndefined();
     });
   });
