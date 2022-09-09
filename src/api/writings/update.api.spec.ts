938c243b186d175cda73476462b05cb28ba53905
CharmingCheol <toby0806@naver.com>
2022-09-09 14:27:08 +0900

    UpdateApi 단일 클래스 추가

---
+++ src/api/writings/update.api.spec.ts
@@ -0,0 +1,43 @@
+import { AxiosError, AxiosResponse } from "axios";
+import { BAD_REQUEST, OK } from "http-status";
+
+import { API_URL } from "constants/api";
+import { WritingFixture } from "fixtures";
+import { HttpMethod } from "types/api";
+import { WritingRequestBody } from "types/writing";
+
+import UpdateApi from "./update.api";
+
+const mockHttpMethod = {
+  patch: jest.fn((entity) => entity),
+};
+
+describe("UpdateApi", () => {
+  const body: WritingRequestBody = { title: "title", content: "content" };
+  const id = "id";
+  let updateApi: UpdateApi;
+
+  beforeEach(() => {
+    const httpMethod = (mockHttpMethod as unknown) as HttpMethod;
+    updateApi = new UpdateApi(httpMethod);
+  });
+
+  it("API 호출 시 body와 id도 같이 전달 된다", async () => {
+    await updateApi.request(body, id);
+    expect(mockHttpMethod.patch).toHaveBeenCalledWith(`${API_URL.WRITINGS}/${id}`, body);
+  });
+
+  it("API 응답이 성공할 경우 전달 받은 데이터를 반환 한다", async () => {
+    const apiSuccess: DeepPartial<AxiosResponse> = { data: new WritingFixture(), status: OK };
+    mockHttpMethod.patch.mockReturnValue(apiSuccess);
+    const actual = await updateApi.request(body, id);
+    expect(actual).toStrictEqual(apiSuccess);
+  });
+
+  it("API 응답이 실패할 경우 에러 데이터를 반환 한다", async () => {
+    const apiError: DeepPartial<AxiosError> = { isAxiosError: true, response: { status: BAD_REQUEST } };
+    mockHttpMethod.patch.mockRejectedValue(apiError);
+    const actual = await updateApi.request(body, id);
+    expect(actual.status).toBe(BAD_REQUEST);
+  });
+});
---
+++ src/api/writings/update.api.ts
@@ -0,0 +1,18 @@
+import { AxiosResponse } from "axios";
+
+import { receiveApiRequest } from "api/utils";
+import { API_URL } from "constants/api";
+import { ApiRequest, HttpMethod } from "types/api";
+import { Writing, WritingRequestBody } from "types/writing";
+
+class UpdateApi implements ApiRequest {
+  constructor(private httpMethod: HttpMethod) {}
+
+  public async request(body: WritingRequestBody, id: string): Promise<AxiosResponse<Writing>> {
+    const api = this.httpMethod.patch(`${API_URL.WRITINGS}/${id}`, body);
+    const response = await receiveApiRequest(api);
+    return response;
+  }
+}
+
+export default UpdateApi;
