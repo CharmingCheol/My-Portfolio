dc051269174c501fa36e2e2083142e63b12a1afb
CharmingCheol <toby0806@naver.com>
2022-09-25 01:51:18 +0900

    create api를 validate, dispatch, receive 전부 하나로 관리

--- src/api/writings/create.api.spec.ts
+++ src/api/writings/create.api.spec.ts
@@ -1,45 +1,119 @@
 import { AxiosError, AxiosResponse } from "axios";
-import { BAD_REQUEST, OK } from "http-status";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from "http-status";
 
 import { API_URL } from "constants/api";
 import { WritingFixture } from "fixtures";
-import { HttpMethod } from "types/api";
+import { globalUIActions } from "reducers/globalUI";
 import { WritingRequestBody } from "types/writing";
 
 import CreateWritingApi from "./create.api";
 
 const mockHttpMethod = {
-  get: jest.fn((entity) => entity),
-  post: jest.fn((entity) => entity),
-  patch: jest.fn((entity) => entity),
-  delete: jest.fn((entity) => entity),
+  post: jest.fn(),
 };
+
+const mockUseHistory = { replace: jest.fn() };
+jest.mock("react-router-dom", () => ({
+  useHistory: jest.fn().mockImplementation(() => mockUseHistory),
+}));
+
+const mockUseDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  useDispatch: jest.fn().mockImplementation(() => mockUseDispatch),
+}));
 
 describe("CreateWritingApi", () => {
   const body: WritingRequestBody = { title: "title", content: "content" };
-  let createWritingApi: CreateWritingApi;
+  let createWritingApi: ReturnType<typeof CreateWritingApi>;
 
   beforeEach(() => {
-    const httpMethod = (mockHttpMethod as unknown) as HttpMethod;
-    createWritingApi = new CreateWritingApi(httpMethod);
+    const httpMethod = mockHttpMethod as any;
+    createWritingApi = CreateWritingApi(httpMethod);
   });
 
-  it("API 호출 시 body로 전달 할 데이터도 같이 전달 된다", async () => {
-    await createWritingApi.request(body);
-    expect(mockHttpMethod.post).toHaveBeenCalledWith(`${API_URL.WRITINGS}`, body);
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
-  it("API 응답이 성공할 경우 전달 받은 데이터를 반환 한다", async () => {
-    const apiSuccess: DeepPartial<AxiosResponse> = { data: new WritingFixture(body), status: OK };
-    mockHttpMethod.post.mockReturnValue(apiSuccess);
-    const actual = await createWritingApi.request(body);
-    expect(actual).toStrictEqual(apiSuccess);
+  describe("validate", () => {
+    it("title이 빈 문자열인 경우 false를 반환 한다", () => {
+      const case1 = createWritingApi.validate({ title: "", content: "" });
+      const case2 = createWritingApi.validate({ title: "  ", content: "" });
+      expect(case1).toBeFalsy();
+      expect(case2).toBeFalsy();
+    });
+
+    it("content가 빈 문자열인 경우 false를 반환 한다", () => {
+      const case1 = createWritingApi.validate({ title: "title", content: "" });
+      const case2 = createWritingApi.validate({ title: "title", content: "  " });
+      expect(case1).toBeFalsy();
+      expect(case2).toBeFalsy();
+    });
+
+    it("데이터가 모두 존재 할 경우 true를 반환 한다", () => {
+      const actual = createWritingApi.validate({ title: "title", content: "content" });
+      expect(actual).toBeTruthy();
+    });
   });
 
-  it("API 응답이 실패할 경우 에러 데이터를 반환 한다", async () => {
-    const apiError: DeepPartial<AxiosError> = { isAxiosError: true, response: { status: BAD_REQUEST } };
-    mockHttpMethod.post.mockRejectedValue(apiError);
-    const actual = await createWritingApi.request(body);
-    expect(actual.status).toBe(BAD_REQUEST);
+  describe("dispatch", () => {
+    it("API 호출 시 body로 전달 할 데이터도 같이 전달 된다", async () => {
+      await createWritingApi.dispatch(body);
+      expect(mockHttpMethod.post).toHaveBeenCalledWith(`${API_URL.WRITINGS}`, body);
+    });
+
+    it("API 응답이 성공할 경우 전달 받은 데이터를 반환 한다", async () => {
+      const apiSuccess: DeepPartial<AxiosResponse> = { data: new WritingFixture(body), status: OK };
+      mockHttpMethod.post.mockReturnValue(apiSuccess);
+
+      const actual = await createWritingApi.dispatch(body);
+      expect(actual).toStrictEqual(apiSuccess);
+    });
+
+    it("API 응답이 실패할 경우 에러 데이터를 반환 한다", async () => {
+      const apiError: DeepPartial<AxiosError> = { isAxiosError: true, response: { status: BAD_REQUEST } };
+      mockHttpMethod.post.mockRejectedValue(apiError);
+
+      const actual = await createWritingApi.dispatch(body);
+      expect(actual).toStrictEqual(apiError.response);
+    });
+  });
+
+  describe("receive", () => {
+    it("status가 200인 경우 게시글 상세 페이지로 이동 한다", () => {
+      const apiSuccess: DeepPartial<AxiosResponse> = { data: new WritingFixture(body), status: OK };
+      createWritingApi.receive(apiSuccess as AxiosResponse);
+      expect(mockUseHistory.replace).toHaveBeenCalledWith(`/writing/${apiSuccess.data.id}`);
+    });
+
+    it("status가 400인 경우 토스트 추가 액션을 호출 한다", () => {
+      const addToastAction = jest.spyOn(globalUIActions, "addToast");
+      const apiFailure: DeepPartial<AxiosResponse> = { status: BAD_REQUEST, statusText: "400 error" };
+
+      createWritingApi.receive(apiFailure as AxiosResponse);
+
+      expect(mockUseDispatch).toHaveBeenCalled();
+      expect(addToastAction).toHaveBeenCalledWith({ message: "400 error" });
+    });
+
+    it("status가 500인 경우 토스트 추가 액션을 호출 한다", () => {
+      const addToastAction = jest.spyOn(globalUIActions, "addToast");
+      const apiFailure: DeepPartial<AxiosResponse> = { status: INTERNAL_SERVER_ERROR, statusText: "500 error" };
+
+      createWritingApi.receive(apiFailure as AxiosResponse);
+
+      expect(mockUseDispatch).toHaveBeenCalled();
+      expect(addToastAction).toHaveBeenCalledWith({ message: "500 error" });
+    });
+
+    it("알 수 없는 status인 경우 토스트 추가 액션을 호출 한다", () => {
+      const addToastAction = jest.spyOn(globalUIActions, "addToast");
+      const apiFailure: DeepPartial<AxiosResponse> = { status: 426, statusText: "426 error" };
+
+      createWritingApi.receive(apiFailure as AxiosResponse);
+
+      expect(mockUseDispatch).toHaveBeenCalled();
+      expect(addToastAction).toHaveBeenCalledWith({ message: "알 수 없는 API 결과 입니다" });
+    });
   });
 });
--- src/api/writings/create.api.ts
+++ src/api/writings/create.api.ts
@@ -1,18 +1,49 @@
-import { AxiosResponse } from "axios";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from "http-status";
 
 import { receiveApiRequest } from "api/utils";
 import { API_URL } from "constants/api";
-import { ApiRequest, HttpMethod } from "types/api";
+import { globalUIActions } from "reducers/globalUI";
+import { StrictApiManager } from "types/api";
 import { Writing, WritingRequestBody } from "types/writing";
 
-class CreateWritingApi implements ApiRequest {
-  constructor(private httpMethod: HttpMethod) {}
+type CreateWritingApi = StrictApiManager<WritingRequestBody, Writing>;
 
-  public async request(body: WritingRequestBody): Promise<AxiosResponse<Writing>> {
-    const api = this.httpMethod.post(`${API_URL.WRITINGS}`, body);
+const createWritingApi: CreateWritingApi = (httpMethod) => ({
+  validate(body) {
+    const { title, content } = body;
+    if (!title?.trim() || !content?.trim()) {
+      return false;
+    }
+    return true;
+  },
+
+  async dispatch(body) {
+    const api = httpMethod.post(`${API_URL.WRITINGS}`, body);
     const response = await receiveApiRequest(api);
     return response;
-  }
-}
+  },
 
-export default CreateWritingApi;
+  receive(response) {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    switch (response.status) {
+      case OK: {
+        history.replace(`/writing/${response.data.id}`);
+        break;
+      }
+      case BAD_REQUEST:
+      case INTERNAL_SERVER_ERROR: {
+        dispatch(globalUIActions.addToast({ message: response.statusText }));
+        break;
+      }
+      default: {
+        dispatch(globalUIActions.addToast({ message: "알 수 없는 API 결과 입니다" }));
+        break;
+      }
+    }
+  },
+});
+
+export default createWritingApi;
