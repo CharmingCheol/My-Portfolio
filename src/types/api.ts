aa48b413dbff5e89ff432687c3eecd464085d407
CharmingCheol <toby0806@naver.com>
2022-09-25 01:50:02 +0900

    api validator, dispatcher, receiver 인터페이스 추가

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,5 +1,17 @@
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 
+interface ApiValidator<A> {
+  validate(args: A): boolean;
+}
+
+interface ApiDispatcher<A, R> {
+  dispatch(args: A): Promise<AxiosResponse<R>>;
+}
+
+interface ApiReceiver<R> {
+  receive(response: AxiosResponse<R>): void;
+}
+
 type ApiResponse = Promise<AxiosResponse<any>>;
 
 export interface HttpMethod {
@@ -18,4 +30,12 @@ export interface ApiInterceptor {
 
 export interface ApiRequest {
   request(...args: any[]): ApiResponse;
+}
+
+export interface LooseApiManager<A, R> {
+  (httpMethod: HttpMethod): ApiDispatcher<A, R> & ApiReceiver<R>;
+}
+
+export interface StrictApiManager<A, R> {
+  (httpMethod: HttpMethod): ApiValidator<A> & ApiDispatcher<A, R> & ApiReceiver<R>;
 }
