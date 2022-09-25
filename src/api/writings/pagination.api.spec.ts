5e020ca7788598c77afca4425075295f4cad462f
CharmingCheol <toby0806@naver.com>
2022-09-25 15:02:41 +0900

    pagination api를 validate, dispatch, receive 전부 하나로 관리

--- src/api/writings/pagination.api.spec.ts
+++ src/api/writings/pagination.api.spec.ts
@@ -3,40 +3,79 @@ import { BAD_REQUEST, OK } from "http-status";
 
 import { API_URL } from "constants/api";
 import { WritingFixture } from "fixtures";
-import { HttpMethod } from "types/api";
 
-import PaginationWritingsApi from "./pagination.api";
+import PaginationWritingApi from "./pagination.api";
+import { writingActions } from "reducers/writing";
 
 const mockHttpMethod = {
   get: jest.fn((entity) => entity),
 };
 
-describe("PaginationWritingsApi", () => {
+const mockUseDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  useDispatch: jest.fn().mockImplementation(() => mockUseDispatch),
+}));
+
+describe("PaginationWritingApi", () => {
   const page = 1;
-  let paginationWritingsApi: PaginationWritingsApi;
+  let paginationWritingApi: ReturnType<typeof PaginationWritingApi>;
 
   beforeEach(() => {
-    const httpMethod = (mockHttpMethod as unknown) as HttpMethod;
-    paginationWritingsApi = new PaginationWritingsApi(httpMethod);
+    const httpMethod = mockHttpMethod as any;
+    paginationWritingApi = PaginationWritingApi(httpMethod);
   });
 
-  it("API 호출 시 page 파라미터가 쿼리스트링으로 추가 된다", async () => {
-    await paginationWritingsApi.request(page);
-    expect(mockHttpMethod.get).toHaveBeenCalledWith(`${API_URL.WRITINGS}`, { params: { page } });
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
-  it("API 응답이 성공할 경우 전달 받은 데이터를 반환 한다", async () => {
-    const writingList = WritingFixture.generateList(10);
-    const apiSuccess: DeepPartial<AxiosResponse> = { data: writingList, status: OK };
-    mockHttpMethod.get.mockReturnValue(apiSuccess);
-    const actual = await paginationWritingsApi.request(page);
-    expect(actual).toStrictEqual(apiSuccess);
+  describe("validate", () => {
+    it("page가 1보다 작은 숫자인 경우 false를 반환 한다", () => {
+      const zero = paginationWritingApi.validate(0);
+      const minus = paginationWritingApi.validate(-1);
+      expect(zero).toBeFalsy();
+      expect(minus).toBeFalsy();
+    });
+
+    it("page가 1이상인 숫장니 경우 true를 반환 한다", () => {
+      const actual = paginationWritingApi.validate(1);
+      expect(actual).toBeTruthy();
+    });
   });
 
-  it("API 응답이 실패할 경우 에러 데이터를 반환 한다", async () => {
-    const apiError: DeepPartial<AxiosError> = { isAxiosError: true, response: { status: BAD_REQUEST } };
-    mockHttpMethod.get.mockRejectedValue(apiError);
-    const actual = await paginationWritingsApi.request(page);
-    expect(actual.status).toBe(BAD_REQUEST);
+  describe("dispatch", () => {
+    it("API 호출 시 page 파라미터가 쿼리스트링으로 추가 된다", async () => {
+      await paginationWritingApi.dispatch(page);
+      expect(mockHttpMethod.get).toHaveBeenCalledWith(`${API_URL.WRITINGS}`, { params: { page } });
+    });
+
+    it("API 응답이 성공할 경우 전달 받은 데이터를 반환 한다", async () => {
+      const writingList = WritingFixture.generateList(10);
+      const apiSuccess: DeepPartial<AxiosResponse> = { data: writingList, status: OK };
+      mockHttpMethod.get.mockReturnValue(apiSuccess);
+
+      const actual = await paginationWritingApi.dispatch(page);
+      expect(actual).toStrictEqual(apiSuccess);
+    });
+
+    it("API 응답이 실패할 경우 에러 데이터를 반환 한다", async () => {
+      const apiError: DeepPartial<AxiosError> = { isAxiosError: true, response: { status: BAD_REQUEST } };
+      mockHttpMethod.get.mockRejectedValue(apiError);
+
+      const actual = await paginationWritingApi.dispatch(page);
+      expect(actual.status).toBe(BAD_REQUEST);
+    });
+  });
+
+  describe("receive", () => {
+    it("status가 200인 경우 게시글 상세 페이지로 이동 한다", () => {
+      const mockAction = jest.spyOn(writingActions, "initWritingPagination");
+      const apiSuccess: DeepPartial<AxiosResponse> = { data: WritingFixture.generateList(10), status: OK };
+
+      paginationWritingApi.receive(apiSuccess as AxiosResponse);
+
+      expect(mockUseDispatch).toHaveBeenCalled();
+      expect(mockAction).toHaveBeenCalledWith(apiSuccess.data);
+    });
   });
 });
--- src/api/writings/pagination.api.ts
+++ src/api/writings/pagination.api.ts
@@ -1,18 +1,34 @@
-import { AxiosResponse } from "axios";
+import { useDispatch } from "react-redux";
+import { OK } from "http-status";
 
 import { receiveApiRequest } from "api/utils";
 import { API_URL } from "constants/api";
-import { ApiRequest, HttpMethod } from "types/api";
+import { writingActions } from "reducers/writing";
+import { StrictApiManager } from "types/api";
 import { WritingPagination } from "types/writing";
 
-class PaginationWritingsApi implements ApiRequest {
-  constructor(private httpMethod: HttpMethod) {}
+type PaginationWritingApi = StrictApiManager<number, WritingPagination>;
 
-  public async request(page: number): Promise<AxiosResponse<WritingPagination>> {
-    const api = this.httpMethod.get(`${API_URL.WRITINGS}`, { params: { page } });
+const paginationWritingApi: PaginationWritingApi = (httpMethod) => ({
+  validate(page) {
+    return 0 < page;
+  },
+
+  async dispatch(page) {
+    const api = httpMethod.get(`${API_URL.WRITINGS}`, { params: { page } });
     const response = await receiveApiRequest(api);
     return response;
-  }
-}
+  },
 
-export default PaginationWritingsApi;
+  receive(response) {
+    const dispatch = useDispatch();
+    switch (response.status) {
+      case OK: {
+        dispatch(writingActions.initWritingPagination(response.data));
+        break;
+      }
+    }
+  },
+});
+
+export default paginationWritingApi;
