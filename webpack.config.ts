7d59749d85416648c34cbb457b9d648d5f661522
CharmingCheol <toby0806@naver.com>
2022-10-16 22:49:14 +0900

    webpack 경로 수정

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,13 +8,18 @@ const config: webpack.Configuration = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
     alias: {
+      api: path.resolve(__dirname, "./src/api/"),
+      apis: path.resolve(__dirname, "./src/apis/"),
       common: path.resolve(__dirname, "src/common/"),
       components: path.resolve(__dirname, "src/components/"),
       containers: path.resolve(__dirname, "src/containers/"),
+      constants: path.resolve(__dirname, "src/constants/"),
       fireConfig: path.resolve(__dirname, "src/fireConfig/"),
+      fixtures: path.resolve(__dirname, "src/fixtures/"),
       hooks: path.resolve(__dirname, "src/hooks/"),
       pages: path.resolve(__dirname, "src/pages/"),
       reducers: path.resolve(__dirname, "src/reducers/"),
+      services: path.resolve(__dirname, "src/services/"),
       static: path.resolve(__dirname, "src/static/"),
       store: path.resolve(__dirname, "src/store/"),
       styles: path.resolve(__dirname, "src/styles/"),
