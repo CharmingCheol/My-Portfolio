906942203eb468e990b55a4208e8bc120005b277
CharmingCheol <toby0806@naver.com>
2022-10-16 22:48:39 +0900

    storybook store 제거 및 apis 경로 추가

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,17 @@
 const path = require("path");
 
 module.exports = ({ config }) => {
-  config.node = {
-    ...config.node,
-    fs: "empty",
-    net: "empty",
-  };
   config.resolve.alias = {
     ...config.resolve.alias,
+    apis: path.resolve(__dirname, "../src", "apis"),
     common: path.resolve(__dirname, "../src", "common"),
     components: path.resolve(__dirname, "../src", "components"),
+    constants: path.resolve(__dirname, "../src", "constants"),
     containers: path.resolve(__dirname, "../src", "containers"),
     fireConfig: path.resolve(__dirname, "../src", "fireConfig"),
+    fixtures: path.resolve(__dirname, "../src", "fixtures"),
     hooks: path.resolve(__dirname, "../src", "hooks"),
+    services: path.resolve(__dirname, "../src", "services"),
     pages: path.resolve(__dirname, "../src", "pages"),
     reducers: path.resolve(__dirname, "../src", "reducers"),
     static: path.resolve(__dirname, "../src", "static"),
@@ -21,6 +20,5 @@ module.exports = ({ config }) => {
     types: path.resolve(__dirname, "../src", "types"),
     utils: path.resolve(__dirname, "../src", "utils"),
   };
-
   return config;
 };
