01e392815a428d6c243f76c7b50d0a38e332c11c
CharmingCheol <toby0806@naver.com>
2022-10-26 00:06:37 +0900

    cypress.config.ts 추가

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    env: {
+      responseImage: "https://res.cloudinary.com/demo/image/upload/w_400/sofa_cat.jpg",
+      serverUrl: "http://localhost:3001/api",
+    },
+  },
+});
