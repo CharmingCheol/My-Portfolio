1954b23e4bdcdd4a8066d55eef608d1fe00f1165
CharmingCheol <toby0806@naver.com>
2022-10-30 23:44:24 +0900

    cypress.config 파일에 supportFile 속성 추가

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
       responseImage: "https://res.cloudinary.com/demo/image/upload/w_400/sofa_cat.jpg",
       serverUrl: "http://localhost:3001/api",
     },
+    supportFile: false,
   },
 });
